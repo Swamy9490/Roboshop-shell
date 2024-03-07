5ab80f95bb2105dc2fc10e8561d8ad308e5f749a
unknown <vvdswamy7479@gmail.com>
2024-03-07 19:39:32 +0530

    roboshop-shell scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -13,7 +13,7 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
     echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
-    if [ $1 -ne 0]
+    if [ $1 -ne 0 ]
     then
         echo -e "$2... $R Failed $N"
         exit 1 
