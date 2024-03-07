a5bb6cc16903f38f3795d9bb3b5aca46bbee486d
unknown <vvdswamy7479@gmail.com>
2024-03-07 18:08:25 +0530

    roboshop-shell scripts

--- shipping.sh
+++ shipping.sh
@@ -1,4 +1,93 @@
 #!/bin/bash
 
 ID=$(id -u)
-R="\e[
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+    echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2... $R Failed $N"
+        exit 1
+    else
+        echo -e "$2... $G Success $N"
+    fi # fi means reverse of if indicating condition end
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R Error:: Please run this script with root access $N"
+    exit 1 # You can give greater then 0
+else
+    echo "you are root user"
+fi # fi menas reverse of if indicating condition end
+
+dnf install maven -y &>> $LOGFILE
+
+id roboshop # if roboshop user does not exist, then it is failure
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y SKIPPING $N"
+fi # fi menas reverse of if indicating condition end
+
+mkdir -p /app 
+
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading shipping"
+
+cd /app
+
+VALIDATE $? "moving to app directory"
+
+unzip -o /tmp/shipping.zip &>> $LOGFILE 
+
+VALIDATE $? "Unzipping shipping"
+
+mvn clean package &>> $LOGFILE
+
+VALIDATE $? "installing dependencies"
+
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
+
+VALIDATE $? "renaming jar file"
+
+cp /home/centos/roboshop-shell/shipping.service/ /etc/systemd/system/shipping.service &>> $LOGFILE
+
+VALIDATE $? "copying shipping service"
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable shipping &>> $LOGFILE
+
+VALIDATE $? "Enable shipping"
+
+systemctl start shipping &>> $LOGFILE
+
+VALIDATE $? "Start shipping"
+
+dnf install mysql -y &>> $LOGFILE
+
+VALIDATE $? "install MySQl client"
+
+mysql -h mysql.swamydevops.cloud -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+
+VALIDATE $? "loading shipping data"
+
+systemctl restart shipping &>> $LOGFILE
+
+VALIDATE $? "restart shipping"
