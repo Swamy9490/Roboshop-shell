32515948b873169526f6080b43d9c8700ccc57d1
unknown <vvdswamy7479@gmail.com>
2024-06-23 11:48:00 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-AMI=ami-0f3c7d07486cad139
-SG_ID=sg-0e7301d568352c84b
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-001cda5bd72c9c47f
 INSTANCES=("mongodb" "mysql" "redis" "rabbitmq" "user" "cart" "shipping" "catalogue" "payment" "dispatch" "web")
 ZONE_ID=Z06751621Z774PSC7OOZZ
 DOMAIN_NAME="swamydevops.cloud"
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE  --security-group-ids sg-0e7301d568352c84b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE  --security-group-ids sg-001cda5bd72c9c47f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
     aws route53 change-resource-record-sets \
